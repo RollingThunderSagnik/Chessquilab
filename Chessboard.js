98dfbb76f2359e421adb95c0e0fa272c97150771
Sagnik Ganguly <sagnikg2013@gmail.com>
2020-08-30 21:22:59 +0530

    ekhon sob fixed

--- Chessboard.js
+++ Chessboard.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { StatusBar, StyleSheet, Text, View, Dimensions, ImageBackground, TouchableWithoutFeedback} from 'react-native';
-// import PropTypes from 'prop-types';
-// import Pospos from './Pospos';
-import Piece from './Piece';
+import PropTypes from 'prop-types';
+import { EventRegister} from 'react-native-event-listeners';
 
 const chsize = Math.floor(Math.min(Math.round(Dimensions.get('window').width), Math.round(Dimensions.get('window').width))/100)*100
-// const image = require('./assets/chessboard.png')//{ uri: "https://reactjs.org/logo-og.png" };
-// const tsize = chsize/8;
+const image = require('./assets/chessboard.png')//{ uri: "https://reactjs.org/logo-og.png" };
+const tsize = chsize/8;
 
 var positions = [
     {id: 1,
@@ -75,129 +74,156 @@ var positions = [
     },
 ]
 
-// class Pospos extends Component {
-//   state = {
-//     left: this.props.left,
-//     top: this.props.top
-//   }
-//   constructor(props)
-//   {
-//     super(props);
-//     this._onConfirmMove = this._onConfirmMove.bind(this);
-//   }
+class Pospos extends Component {
+  state = {
+    left: this.props.left,
+    top: this.props.top
+  }
+  constructor(props)
+  {
+    super(props);
+  }
 
-//   _onConfirmMove(){
-//   }
-
-//   render()
-//   {
-//     return (
-//       <TouchableWithoutFeedback onPress={() => this.props.onPress(this.state.left,this.state.top)}>
-//       <View style={{
-//         borderColor : '#000',
-//         borderWidth: 1,
-//         borderRadius: tsize*0.5,
-//         margin: 0.25*tsize,
-//         padding: 0,
-//         backgroundColor: '#09f09f',
-//         width: 0.5*tsize,
-//         height: 0.5*tsize,
-//         position: "absolute",
-//         left: this.state.left*tsize,
-//         top: this.state.top*tsize,
-//         zIndex: 99
-//       }}> 
-//       </View>
-//       </TouchableWithoutFeedback>
-//     );
-
-//   }
-// }
-
-// Pospos.propTypes = { onPress: PropTypes.func.isRequired };
-
-// class Piece extends Component {
-//   state = {
-//     left: this.props.left,
-//     top: this.props.top,
-//     nextmove: []
-//   }
-//   constructor(props)
-//   {
-//     super(props);
-//     this._onPressSend = this._onPressSend.bind(this);
-//     this._Move = this._Move.bind(this);
-//   }
-
-  
-//   _onPressSend(){
-//     // this.setState({top : this.state.top-1});
-//     // console.log(this.state.messages);
-     
-//       var moves = [<Pospos onPress={this._Move} key={0} top={this.state.top-1} left={this.state.left}></Pospos>,<Pospos onPress={this._Move} key={1} top={this.state.top-2} left={this.state.left}></Pospos>]
-//       this.setState({nextmove: moves});
-//     }
-
-//   _Move(x,y){
-//     this.setState({
-//       left: x,
-//       top: y,
-//       nextmove : []
-//     });
-//     // alert(this.props.id);
-//   }  
-
-//   render()
-//   {
-//     return (
-//       <View>
-//         <TouchableWithoutFeedback onPress={this._onPressSend}>
-//           <View style={{
-//             margin: 0,
-//             padding: 0,
-//             backgroundColor: '#09f',
-//             width: tsize,
-//             height: tsize,
-//             position: "absolute",
-//             left: this.state.left*tsize,
-//             top: this.state.top*tsize
-//           }}>
-//             <Text>{this.state.left + "," + this.state.top+"\n"+this.props.id}</Text>
-//           </View>
-//         </TouchableWithoutFeedback>
-//         {this.state.nextmove}
-//       </View>
-//     );
-
-//   }
-// }
-
-class Chessboard extends Component {
   render()
   {
-    // var pisces = [];
-    // for(let i=0;i<=7;i++)
-    // {
-    //   pisces.push(<Piece key={i} id={i} left={i} top={6}></Piece>)
-    //   pisces.push(<Piece key={8+i} id={8+i} left={i} top={7}></Piece>)
-    // }
+    return (
+      <TouchableWithoutFeedback onPress={() => this.props.onPress(this.state.left,this.state.top)}>
+      <View style={{
+        borderColor : '#000',
+        borderWidth: 1,
+        borderRadius: tsize*0.5,
+        margin: 0.25*tsize,
+        padding: 0,
+        backgroundColor: '#09f09f',
+        width: 0.5*tsize,
+        height: 0.5*tsize,
+        position: "absolute",
+        left: this.state.left*tsize,
+        top: this.state.top*tsize,
+        zIndex: 99
+      }}> 
+      </View>
+      </TouchableWithoutFeedback>
+    );
 
+  }
+}
+Pospos.propTypes = { onPress: PropTypes.func.isRequired };
+
+class Piece extends Component {
+  state = {
+    left: this.props.left,
+    top: this.props.top,
+    nextmove: []
+  }
+
+  constructor(props)
+  {
+    super(props);
+    this._onShowMoves = this._onShowMoves.bind(this);
+    this._Move = this._Move.bind(this);
+  }
+
+  componentDidMount() {
+    this.listener = EventRegister.addEventListener('clearpos', () => {
+        this.setState({
+            nextmove: []
+        });
+    });
+  }
+
+  componentWillUnmount() {
+    EventRegister.removeEventListener(this.listener);
+  }
+
+  _onShowMoves(){
+      EventRegister.emit('clearpos');
+      var move =[{x: this.state.left, y:this.state.top-1},{x: this.state.left, y:this.state.top-2}]
+      var validmoves=[];
+      for(let i=0;i<move.length;i++)
+      {
+        var indx = (positions.findIndex( (piece) => {return ((piece.x==move[i].x)&&(piece.y==move[i].y))}));
+        if(indx == -1)
+          validmoves.push(move[i]);
+      }
+      var moves = validmoves.map((cood) => <Pospos onPress={this._Move} key={validmoves.indexOf(cood)} top={cood.y} left={cood.x}></Pospos>);
+     this.setState({nextmove: moves});
+    }
+
+    componentDidUpdate(prevProps) {
+      if (this.props.left !== prevProps.left || this.props.top !== prevProps.top) {
+         this.setState({ 
+           left : this.props.left,
+           top : this.props.top,
+           }
+         );
+       }
+    }
+    
+  _Move(x,y){
+    this.setState({
+      left: x,
+      top: y,
+      nextmove : []
+    });
+  }  
+
+  render()
+  {
+    return (
+      <View>
+      <TouchableWithoutFeedback onPress={this._onShowMoves}>
+      <View style={{
+        margin: 0,
+        padding: 0,
+        backgroundColor: '#09f',
+        width: tsize,
+        height: tsize,
+        position: "absolute",
+        left: this.state.left*tsize,
+        top: this.state.top*tsize
+      }}>
+        <Text>{this.state.left + "," + this.state.top+"\n"+this.props.id}</Text>
+      </View>
+      </TouchableWithoutFeedback>
+      {this.state.nextmove}
+      </View>
+    );
+
+  }
+}
+
+class Chessboard extends Component {
+
+  constructor(props)
+  {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.listener = EventRegister.addEventListener('movemade', () => {
+    });
+  }
+
+  componentWillUnmount() {
+    EventRegister.removeEventListener(this.listener);
+  }
+
+  render()
+  {
     var pisces = positions.map( (item) => <Piece key={item.id} id={item.id} left={item.x} top={item.y}></Piece>);
 
     return (
       
     <View style={{
-      marginTop: 220,
-      marginLeft: 28,
       width: chsize,
-      height: chsize,
-      justifyContent: 'center',
       backgroundColor: '#09f',
+      height: chsize,
       alignItems : 'center',
       }}>
       
       <ImageBackground source={require('./assets/chessboard.png')} style={styles.image}>
-        {pisces}
+      {pisces}
       </ImageBackground>
     </View>
     );
