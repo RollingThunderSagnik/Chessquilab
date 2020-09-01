f66d8681352af67681f6eba75252d5295b0baec1
i-Rony <ronrocks999@gmail.com>
2020-09-02 05:21:36 +0530

    Fine Sign Up auth khelche

--- SignUp.js
+++ SignUp.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
+import {f, auth, database} from './config/config';
 
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
@@ -14,25 +15,68 @@ const SignUp = ({ navigation }) => {
     });
 
     const [data, setData] = useState({
-        username: '',
+        fullname: '',
+        email: '',
         password: '',
         confirm_password: '',
         secureTextEntry: true,
         confirm_secureTextEntry: true,
+        signedUp: false
     });
+
+    const SignUpUser = async(email, password, confirm_password, fullname) => {
+        if(password === confirm_password){
+            if (email != '' && password != ''){                
+                await auth.createUserWithEmailAndPassword(email, password)
+                .then((result) => {
+                    return result.user.updateProfile({
+                      displayName: fullname
+                    })
+                }).then(() => console.log(auth.currentUser.displayName))
+                .catch((error) => console.log('error logging in', error)); 
+                setData({
+					...data,
+					signedUp: true
+				});
+            } else {
+                setData({
+					...data,
+					signedUp: false
+                });
+                alert('Invalid Username or Password');
+            }
+        } else {
+            setData({
+                ...data,
+                signedUp: false
+            });
+            alert('Passwords do not match');
+        }
+    };
+
+    if (data.signedUp === true){
+        navigation.navigate('Menu');
+    }
 
     const textInputChange = (val) => {
         if (val.length !== 0) {
             setData({
                 ...data,
-                username: val,
+                email: val,
             });
         } else {
             setData({
                 ...data,
-                username: val,
+                email: val,
             });
         }
+    }
+
+    const fullnameInputChange = (val) => {
+        setData({
+            ...data,
+            fullname: val
+        });
     }
 
     const handlePasswordChange = (val) => {
@@ -85,7 +129,31 @@ const SignUp = ({ navigation }) => {
                         size={20}
                     />
                     <TextInput
-                        
+                        placeholder='Full Name'
+                        style={{
+                            color: 'white',
+                            fontFamily: 'Carme',
+                            flex: 0.8,
+                            marginLeft: 10,
+                            borderBottomWidth: 1,
+                            borderBottomColor: 'white',
+                        }}
+                        keyboardType='default'
+                        selectionColor='white'
+                        autoCapitalize='none'
+                        onChangeText={(val) => fullnameInputChange(val)}
+                    />                    
+                </View>
+
+                <View
+                    style={{ paddingVertical: 10, flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center'}}
+                >
+                    <FontAwesome
+                        name="user-o"
+                        color="white"
+                        size={20}
+                    />
+                    <TextInput                        
                         placeholder='Email Address'
                         style={{
                             color: 'white',
@@ -200,7 +268,7 @@ const SignUp = ({ navigation }) => {
                         borderRadius: 20,
                         marginTop: 40
                     }}
-                    onPress={() => navigation.navigate('Menu')}
+                    onPress={() => SignUpUser(data.email, data.password, data.confirm_password, data.fullname)}
                 >
                     <Text
                         style={{
