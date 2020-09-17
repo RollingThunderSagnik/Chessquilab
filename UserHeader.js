d70919f32959fa1f37cdc4a5f62fb6cfbc708420
i-Rony <ronrocks999@gmail.com>
2020-09-18 04:10:51 +0530

    UserHeader UI

--- UserHeader.js
+++ UserHeader.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import { View, Text, Dimensions, Image, TouchableOpacity } from 'react-native';
 import {f, auth, database} from './config/config';
-const { width, height } = Dimensions.get('screen');
 import Feather from 'react-native-vector-icons/Feather';
+import userimagepawn from './assets/userimagepawn.png';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-
+const { width, height } = Dimensions.get('screen');
 
 class UserHeader extends Component {
     state = {
@@ -39,9 +40,9 @@ class UserHeader extends Component {
                 // marginHorizontal: 12
             },
         }
-        this.state.matches = 10;
-        this.state.wins = 10;
-        this.state.losses = 10;
+        this.state.matches = 40;
+        this.state.wins = 20;
+        this.state.losses = 20;
         this.state.witch = true;
     }
 
@@ -122,17 +123,20 @@ class UserHeader extends Component {
                 alignItems: 'center',
                 backgroundColor: '#181818',
                 paddingVertical: 12,
-                margin: 0
+                margin: 0,
+                flex: 0.2
             }}
         >
-            <View style={{
-                flex:1,
-                // backgroundColor: '#000',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}>
-            {/* https://instagram.fccu10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/118882303_650093682314195_6731059514992743719_n.jpg?_nc_ht=instagram.fccu10-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=ReBfetCx4zoAX_Kxa8M&oh=a4bebcabee73008ea9d7a81757164a75&oe=5F7FD638 */}
-            <Image source={{uri: 'https://www.pngitem.com/pimgs/m/537-5372558_flat-man-icon-png-transparent-png.png'}} 
+            <View style={{flex: 0.33, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{fontFamily: 'Carme', color: 'white', fontSize: 30}}>{this.state.wins}</Text>
+                <FontAwesome 
+                    name='trophy' 
+                    color='white' 
+                    size={22}
+                />
+            </View>
+            <View style={{flex: 0.34, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between'}}>
+            <Image source={userimagepawn} 
                 style={{
                     padding: 4,
                     resizeMode: 'cover', 
@@ -142,56 +146,17 @@ class UserHeader extends Component {
                     margin: 0
                 }} 
             />
+            <Text numberOfLines={2} allowFontScaling adjustsFontSizeToFit style={{fontFamily: 'Carme', color: 'white', fontSize: 20}}>{this.state.name}</Text>
             </View>
-           
-            <View 
-            style={{
-                flex:2,
-                paddingLeft: 10
-                // backgroundColor: '#181818',
-                // justifyContent: 'center',
-                // alignItems: 'flex-start',
-            }}>
-                <View 
-                style={{
-                    flexDirection: 'column',
-                    // alignItems: 'center',
-                    // justifyContent: 'space-evenly' 
-                }}
-            >
-                <Text 
-                    allowFontScaling 
-                    numberOfLines={1} 
-                    style={{
-                        color: 'white', 
-                        fontFamily: 'Carme', 
-                        fontSize: 20,
-                        // alignSelf: 'flex-start',
-                        marginBottom: 10,
-                        // backgroundColor: '#444'
-                    }}
-                >
-                    {this.state.name}
-                </Text>
-                <View style={{flexDirection: 'row',
-                // justifyContent: 'space-between'
-                }}>
-                    <View style={this.state.styles.viu}>
-                        <Text style={this.state.styles.text}>{this.state.wins}</Text>
-                        <Text style={this.state.styles.label}>{'WINS'}</Text>
-                    </View>
-                    <View style={this.state.styles.viu}>
-                        <Text style={this.state.styles.text}>{this.state.losses}</Text>
-                        <Text style={this.state.styles.label}>{'LOSSES'}</Text>
-                    </View>
-                    <View style={this.state.styles.viu}>
-                        <Text style={this.state.styles.text}>{this.state.losses}</Text>
-                        <Text style={this.state.styles.label}>{'LOSSES'}</Text>
-                    </View>
-                </View>
-            </View>
-            </View>
-        
+
+            <View style={{flex: 0.33, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                <Text style={{fontFamily: 'Carme', color: 'white', fontSize: 30}}>{this.state.matches}</Text>
+                <FontAwesome 
+                    name='gamepad' 
+                    color='white' 
+                    size={22}
+                />
+            </View>        
         </View>
         </>
         )
